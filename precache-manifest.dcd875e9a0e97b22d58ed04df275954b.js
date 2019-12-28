05041041a17b80b84b197b49188b3c674008e069
Jeffrey Emakpor <jeffreyefemena4@gmail.com>
2019-12-28 03:31:18 +0100

    first pushing of production tutorial react app

---
+++ precache-manifest.dcd875e9a0e97b22d58ed04df275954b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80ee102e8190067ab9084bb36a764bea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd1bff4a16c25cc19ffd",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a244d43816a0c0e35773",
+    "url": "/static/js/2.bcdb03a1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.bcdb03a1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd1bff4a16c25cc19ffd",
+    "url": "/static/js/main.5a77f287.chunk.js"
+  },
+  {
+    "revision": "52e3bf1e2b6623dd5682",
+    "url": "/static/js/runtime-main.e8c02d48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
